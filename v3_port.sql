0b186d4ff17f93eb275cea97336b9edfabbd8f25
L Carl Pedersen <lcarlp@users.noreply.github.com>
2020-07-15 17:13:56 -0400

    Do not generate the file to clear the problem list.  Needed only to correct errors.

--- v3_port.sql
+++ v3_port.sql
@@ -5,8 +5,6 @@
 select * from v3_encounters;
 .once v3_port/v3_status_update.csv
 select * from v3_status_update;
-.once v3_port/v3_problem_list_clear.csv
-select * from v3_problem_list_clear;
 .once v3_port/v3_problem_list.csv
 select * from v3_problem_list;
 .once v3_port/v3_primary_referrer.csv
